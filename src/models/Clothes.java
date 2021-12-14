92c27371fc077dbfa9ccc174b0b07c98217b37a4
Large <james.large@accenture.com>
2021-12-14 15:16:43 +0000

    Clothes array list

---
+++ src/models/Clothes.java
@@ -0,0 +1,24 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Clothes {
+    public String item;
+    public int id;
+    public int price;
+    public int quantity;
+
+    private ArrayList<Clothes> clothesArrayList;
+    private Clothes clothes;
+
+    public Clothes(String item, int id, int price, int quantity) {
+        this.item = item;
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+
+
+}
+
